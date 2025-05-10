55f7a8afb1abe3cd026712dc717b6285cdc48724
nascimento3322 <vn62511@gmail.com>
2025-05-10 16:35:24 -0300

    Subtraindo

--- code.py
+++ code.py
@@ -5,3 +5,9 @@ a = int(input("Digite um valor: "))
 b = int(input("Digite mais um valor: "))
 soma = a + b 
 print(f"O resultado final dessa soma é: {soma} ")
+
+#Calculo de subtração
+c = int(input("Digite um número: "))
+d = int(input("Digite outro número: "))
+subtração = c - d 
+print(f"O resultado final da subtração é: {subtração}")
