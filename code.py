a32d2e948a4060d8ed3dc0ef901c5c84a3421ad7
nascimento3322 <vn62511@gmail.com>
2025-05-10 16:29:20 -0300

    Somando

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
 print("Está é uma calculadora nova")
+
+#Calculo de soma
+a = int(input("Digite um valor: "))
+b = int(input("Digite mais um valor: "))
+soma = a + b 
+print(f"O resultado final dessa soma é: {soma} ")
